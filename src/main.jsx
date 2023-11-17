8768e782bfd7c41dee992b45e189120d4753fd2d
wesioC <wesio.coelho@estudante.ifgoiano.edu.br>
2023-11-17 15:17:16 -0300

    some fixes

--- src/main.jsx
+++ src/main.jsx
@@ -7,42 +7,33 @@ import { Theme } from '@radix-ui/themes';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from './routes/Home.jsx';
-import Loan from './routes/Loan.jsx'
-import Tool from './routes/Tool.jsx'
-import User from './routes/User.jsx'
+import Loan from './routes/Loan.jsx';
+import Tool from './routes/Tool.jsx';
+import User from './routes/User.jsx';
 import Header from './components/header/Header.jsx';
-import TableTool from './components/tabletool/TableTool.jsx'
-import TableLoan from './components/tableloan/TableLoan.jsx'
+
 
 const router = createBrowserRouter([
   {
-  path: "/", element: <Home />
-  }, 
+    path: "/", element: <Home />
+  },
   {
     path: "Loan", element: <Loan />
-  }, 
+  },
   {
     path: "Tool", element: <Tool />
-  }, 
+  },
   {
     path: "User", element: <User />
   },
-  {
-    path: "TableTool", element: <TableTool />
-  },
-  {
-    path: "TableLoan", element: <TableLoan />
-  },
-  {
-    path: "Header", element: <Header />
-  },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Theme>
-    <Header/>
-      <RouterProvider router={router}/>
+      <Header />
+      <RouterProvider router={router} />
     </Theme>
   </React.StrictMode>,
 )
