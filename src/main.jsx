bc1994d55dbf193686c5126daacaeef86d85490e
wesioC <wesio.coelho@estudante.ifgoiano.edu.br>
2023-11-17 15:09:08 -0300

    Header pra baixo

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,11 @@ import { Theme } from '@radix-ui/themes';
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Header from './components/header/Header.jsx';
 import Home from './routes/Home.jsx';
 import Loan from './routes/Loan.jsx'
 import Tool from './routes/Tool.jsx'
 import User from './routes/User.jsx'
+import Header from './components/header/Header.jsx';
 import TableTool from './components/tabletool/TableTool.jsx'
 import TableLoan from './components/tableloan/TableLoan.jsx'
 
@@ -28,20 +28,20 @@ const router = createBrowserRouter([
     path: "User", element: <User />
   },
   {
-    path: "TableTool", element: <TableTool/>
+    path: "TableTool", element: <TableTool />
   },
   {
-    path: "TableLoan", element: <TableLoan/>
+    path: "TableLoan", element: <TableLoan />
   },
   {
-    path: "Header", element: <Header/>
+    path: "Header", element: <Header />
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Theme >
-      <Header/>
+    <Theme>
+    <Header/>
       <RouterProvider router={router}/>
     </Theme>
   </React.StrictMode>,
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,13 +4,17 @@ import '@radix-ui/themes/styles.css';
 import { Flex, Table, Button } from '@radix-ui/themes';
 import Tabletool from '../components/tabletool/TableTool';
 import TableLoan from '../components/tableloan/TableLoan';
+import Header from '../components/header/Header';
 
 const Home = () => {
   return (
+    
     <Flex direction="column" gap="6" right="3" align="center">
+      
     <div>
         <input type='text' id='busca' placeholder='Buscar ferramenta...' />
     </div>
+    
     <Tabletool />
     <div className="responsive-text">
         <h4>Materiais com devolução próximo ao vencimento:</h4>
